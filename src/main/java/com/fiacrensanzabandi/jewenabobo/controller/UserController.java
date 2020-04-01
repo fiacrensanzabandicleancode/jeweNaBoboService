01b9f18b6b7cb85a436ef3c01cf6edd06b3deb0b
NSANZABANDI Fiacre <fiacre.nsanzabandi@soprasteria.com>
2020-04-01 23:10:57 +0200

    Added crossorigin on my controller.

--- src/main/java/com/fiacrensanzabandi/jewenabobo/controller/UserController.java
+++ src/main/java/com/fiacrensanzabandi/jewenabobo/controller/UserController.java
@@ -5,6 +5,7 @@ import javax.validation.constraints.NotNull;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -18,6 +19,7 @@ import com.fiacrensanzabandi.jewenabobo.service.UserService;
  * @author fnsanzabandi
  *
  */
+@CrossOrigin
 @RestController
 public class UserController {
 	
