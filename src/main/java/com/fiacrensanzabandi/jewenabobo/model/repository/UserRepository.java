197f2723ba47728ac99c590bd13cff9b2810a3b0
NSANZABANDI Fiacre <fiacre.nsanzabandi@soprasteria.com>
2020-04-11 10:15:46 +0200

    Added some comments on the user repository.

--- src/main/java/com/fiacrensanzabandi/jewenabobo/model/repository/UserRepository.java
+++ src/main/java/com/fiacrensanzabandi/jewenabobo/model/repository/UserRepository.java
@@ -10,13 +10,27 @@ import com.fiacrensanzabandi.jewenabobo.model.User;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
-    
+
+	/**
+	 * Returns specified users with the name passed in parameter.
+	 * 
+	 * @param name
+	 * @return User
+	 * @author fnsanzabandi
+	 */
 	static Specification<User> hasName(String name) {
 		return (user, criteriaQuery, criteriaBuilder) -> criteriaBuilder.equal(user.get("name"), name);
 	}
-	
+
+	/**
+	 * Returns specified users with the password in parameter.
+	 * 
+	 * @param password
+	 * @return User
+	 * @author fnsanzabandi
+	 */
 	static Specification<User> hasPassword(String password) {
 		return (user, cq, cb) -> cb.equal(user.get("password"), password);
 	}
-		
+
 }
