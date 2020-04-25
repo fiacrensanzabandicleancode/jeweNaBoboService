c9f0a8e6c510f64b7328e308a86e47dbf54e02ce
NSANZABANDI Fiacre <fiacre.nsanzabandi@soprasteria.com>
2020-04-25 20:31:24 +0200

    Did a little clean.

--- src/main/java/com/fiacrensanzabandi/jewenabobo/model/repository/UserRepository.java
+++ src/main/java/com/fiacrensanzabandi/jewenabobo/model/repository/UserRepository.java
@@ -3,7 +3,6 @@ package com.fiacrensanzabandi.jewenabobo.model.repository;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.fiacrensanzabandi.jewenabobo.model.User;
